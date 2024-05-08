1009c1994bac0ac03b2b3374161965d1563289c5
Ashish Garg <ashish.g@royalcyber.com>
2024-05-08 14:24:01 +0530

    Changed routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
 } from "react-router-dom";
 
 import ProductDescriptionPage from "./Pages/productDescription/ProductDescriptionPage";
@@ -11,21 +10,27 @@ import ProductListingPage from "./Pages/productListing/ProductListingPage";
 import Header from "./components/header/Header";
 
 function App() {
-	return (
-		<>
-		<Header></Header>
-		<Router>
-			<Routes>
-				<Route path="/" element={<Navigate to="/ProductListingPage" />} />
-				<Route path="/ProductListingPage" element={<ProductListingPage />} />
-				<Route
-					path="/ProductDescriptionPage/:productId"
-					element={<ProductDescriptionPage />}
-				/>
-			</Routes>
-		</Router>
-		</>
-	);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navigate to="/ProductListingPage" />,
+    },
+    {
+      path: "/ProductListingPage",
+      element: <ProductListingPage />,
+    },
+    {
+      path: "/ProductDescriptionPage/:productId",
+      element: <ProductDescriptionPage />,
+    },
+  ]);
+
+  return (
+    <>
+      <Header />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
