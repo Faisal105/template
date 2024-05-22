2ac409c288d0cc8898c8a82b435a314e97ef8b40
tejeshpatil25 <tejeshpatil145@gmail.com>
2024-05-22 11:30:57 +0530

    notification component upddated

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -1,34 +1,3 @@
-// import React from 'react';
-
-// const Notification = ({ message, type, onClose }) => {
-//     const getBackgroundColor = () => {
-//         switch (type) {
-//             case 'success':
-//                 return 'bg-green-500';
-//             case 'info':
-//                 return 'bg-blue-500';
-//             case 'warning':
-//                 return 'bg-yellow-500';
-//             case 'error':
-//                 return 'bg-red-500';
-//             default:
-//                 return 'bg-gray-500';
-//         }
-//     };
-
-//     return (
-//         <div className={`fixed top-4 right-4 p-4 rounded-md text-white ${getBackgroundColor()}`}>
-//             {message}
-//             <button onClick={onClose} className="ml-4">
-//                 ✖
-//             </button>
-//         </div>
-//     );
-// };
-
-// export default Notification;
-
-
 import React from 'react';
 
 const Notification = ({ message, type, onClose }) => {
