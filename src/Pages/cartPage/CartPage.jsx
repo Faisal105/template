20e357112c107169f21b01884a649c45dab5ce91
Faisal bashir <faisalbashir517@gmail.com>
2024-05-27 15:09:33 +0400

    ui changes home page

--- src/Pages/cartPage/CartPage.jsx
+++ src/Pages/cartPage/CartPage.jsx
@@ -1,111 +1,134 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Text from "../../components/text/Text";
 import { useCart } from "../../contexts/CartContext";
 import Image from "../../components/image/Image";
 import Counter from "../../components/counter/Counter";
 import Notification from "../../components/notification/Notification";
 import Button from "../../components/button/Button";
+
 const CartPage = ({ location }) => {
-  const { cartItems, addToCart, removeFromCart, deleteFromCart } = useCart();
-  const [notification, setNotification] = useState(null);
+	const { cartItems, addToCart, removeFromCart, deleteFromCart } = useCart();
+	const [notification, setNotification] = useState(null);
+	const [totalPrice, setTotalPrice] = useState(0);
 
-  const handleIncrease = (product) => {
-    addToCart(product);
-    setNotification({ message: "Quantity updated", type: "info" });
-    setTimeout(() => {
-      setNotification(null);
-    }, 800);
-  };
+	useEffect(() => {
+		const calculateTotalPrice = () => {
+			const total = cartItems.reduce(
+				(sum, item) => sum + item.price * item.quantity,
+				0
+			);
+			setTotalPrice(total);
+		};
+		calculateTotalPrice();
+	}, [cartItems]);
 
-  const handleDecrease = (productID) => {
-    removeFromCart(productID);
-    setNotification({ message: "Quantity updated", type: "info" });
-    setTimeout(() => {
-      setNotification(null);
-    }, 800);
-  };
+	const handleIncrease = (product) => {
+		addToCart(product);
+		setNotification({ message: "Quantity updated", type: "info" });
+		setTimeout(() => {
+			setNotification(null);
+		}, 800);
+	};
 
-  return (
-    <>
-      {notification && (
-        <Notification
-          message={notification.message}
-          type={notification.type}
-          onClose={() => setNotification(null)}
-        />
-      )}
-      <div className="container mx-auto px-4 py-8">
-        <Text customClasses="text-4xl font-bold mb-4">Your Cart</Text>
-        {cartItems.length > 0 ? (
-          <>
-            <table className="mt-8 w-full lg:w-3/5 text-center">
-              <thead>
-                <tr>
-                  <th className="px-5 py-3">Product</th>
-                  <th className="px-5 py-3">Product Name</th>
-                  <th className="px-5 py-3">Price</th>
-                  <th className="px-5 py-3">Qty</th>
-                  <th className="px-5 py-3">Total</th>
-                  <th className="px-5 py-3">Remove</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cartItems?.map((item, index) => (
-                  <tr key={item.id}>
-                    <td className="px-6 py-3">
-                      <Image
-                        src={item.image}
-                        alt={item.title}
-                        customClasses="max-w-full w-16 h-16 object-contain"
-                      />
-                    </td>
-                    <td className="px-6 py-3">{item.title}</td>
-                    <td className="px-5 py-3">${item.price}</td>
-                    <td className="px-5 py-3">
-                      <Counter
-                        quantity={item.quantity}
-                        onIncrease={() => handleIncrease(item)}
-                        onDecrease={() => handleDecrease(item.id)}
-                      />
-                    </td>
-                    <td className="px-5 py-3">${item.price * item.quantity}</td>
-                    <td className="px-5 py-3 text-center">
-                      <button
-                        onClick={() => deleteFromCart(item.id)}
-                        className="text-red-500"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="20"
-                          height="20"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          stroke-width="2"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M5 7h14M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3m0 0H9m0 0H5"
-                          />
-                        </svg>
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </>
-        ) : (
-          <Text customClasses="my-8">
-            Your cart is empty. Start adding some products to it.
-          </Text>
-        )}
+	const handleDecrease = (productID) => {
+		removeFromCart(productID);
+		setNotification({ message: "Quantity updated", type: "info" });
+		setTimeout(() => {
+			setNotification(null);
+		}, 800);
+	};
 
-      </div>
-    </>
-
-  );
+	return (
+		<>
+			{notification && (
+				<Notification
+					message={notification.message}
+					type={notification.type}
+					onClose={() => setNotification(null)}
+				/>
+			)}
+			<div className="container mx-auto px-4 py-8">
+				<Text customClasses="text-4xl font-bold mb-4 text-center">
+					Your Cart
+				</Text>
+				{cartItems.length > 0 ? (
+					<>
+						<table className="mt-8 w-full lg:w-3/5 mx-auto text-center">
+							<thead>
+								<tr>
+									<th className="px-5 py-3">Product</th>
+									<th className="px-5 py-3">Product Name</th>
+									<th className="px-5 py-3">Unit Price</th>
+									<th className="px-5 py-3">Qty</th>
+									<th className="px-5 py-3">Total</th>
+									<th className="px-5 py-3">Remove</th>
+								</tr>
+							</thead>
+							<tbody>
+								{cartItems?.map((item) => (
+									<tr key={item.id} className="border-t border-gray-300">
+										<td className="px-6 py-3">
+											<Image
+												src={item.image}
+												alt={item.title}
+												customClasses="max-w-full w-16 h-16 object-contain mx-auto"
+											/>
+										</td>
+										<td className="px-6 py-3">{item.title}</td>
+										<td className="px-5 py-3">${item.price.toFixed(2)}</td>
+										<td className="px-5 py-3">
+											<Counter
+												quantity={item.quantity}
+												onIncrease={() => handleIncrease(item)}
+												onDecrease={() => handleDecrease(item.id)}
+											/>
+										</td>
+										<td className="px-5 py-3">
+											${(item.price * item.quantity).toFixed(2)}
+										</td>
+										<td className="px-5 py-3 text-center">
+											<button
+												onClick={() => deleteFromCart(item.id)}
+												className="text-red-500">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													width="20"
+													height="20"
+													fill="none"
+													viewBox="0 0 24 24"
+													stroke="currentColor"
+													strokeWidth="2">
+													<path
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M5 7h14M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3m0 0H9m0 0H5"
+													/>
+												</svg>
+											</button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+							<tfoot>
+								<tr>
+									<td colSpan="5" className="text-right font-bold px-5 py-3">
+										Total Price:
+									</td>
+									<td className="px-5 py-3 font-bold">
+										${totalPrice.toFixed(2)}
+									</td>
+								</tr>
+							</tfoot>
+						</table>
+					</>
+				) : (
+					<Text customClasses="my-8 text-center">
+						Your cart is empty. Start adding some products to it.
+					</Text>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default CartPage;
--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -4,119 +4,84 @@ import Image from "../image/Image";
 import { redirect } from "react-router-dom";
 
 const Cart = () => {
-  const { cartItems, isCartOpen, toggleCart, removeFromCart , deleteFromCart } = useCart();
+	const { cartItems, isCartOpen, toggleCart, removeFromCart, deleteFromCart } =
+		useCart();
 
-  const handleOnclick = () => { 
-      redirect('/CartPage')
-  };
+	const handleOnclick = () => {
+		redirect("/CartPage");
+	};
 
-  return (
-    <>
-      <div
-        className={`fixed inset-0 z-50 ${isCartOpen ? 'block' : 'hidden'} bg-black bg-opacity-50`}
-        onClick={toggleCart}
-      />
-      <div
-        className={`fixed top-0 ${isCartOpen ? 'right-0' : '-right-full'} w-[500px] h-full bg-white transition-right duration-300 ease-in-out p-6 box-border z-50`}
-      >
-        <div className="flex justify-between items-center mb-4">
-          <h2>Cart</h2>
-          <button className="text-red-600 text-sm" onClick={toggleCart}>
-            X
-          </button>
-        </div>
-        <hr />
-        <div
-          id="cart"
-          className="flex flex-col justify-between h-[88vh] overflow-auto"
-        >
-          <table className="mt-2">
-            <tbody className="space-y-6">
-              {cartItems.map((item, index) => (
-              <tr key={item.id}>
-                <td className="py-3 pr-3">
-                  <div className="w-16 h-16">
-                    <Image
-                      src={item.image}
-                      alt={item.title}
-                      customClasses="max-w-full max-h-full object-contain"
-                    />
-                  </div>
-                </td>
-                <td className="py-3 px-3">{item.title}</td>
-                <td className="py-3 px-3">x{item.quantity}</td>
-                <td className="py-3 px-3">${(item.price * item.quantity).toFixed(2)}</td>
-                <td className="py-3 pl-3 text-right"> <button
-                    onClick={() => deleteFromCart(item.id)}
-                    className="text-red-500"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M5 7h14M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3m0 0H9m0 0H5"
-                      />
-                    </svg>
-                  </button></td>
-              </tr>
-              
-              ))}
-            </tbody>
-          </table>
-            {/* {cartItems.map((item, index) => (
-              <li
-                key={item.id}
-                className="flex items-center justify-between my-7"
-              >
-                <div className="flex justify-between items-center gap-5 w-full">
-                  <div className="w-16 h-16">
-                    <Image
-                      src={item.image}
-                      alt={item.title}
-                      customClasses="max-w-full max-h-full object-contain"
-                    />
-                  </div>
-                  <span>{item.title}</span>
-                  <span>x{item.quantity}</span>
-                  <span>${(item.price * item.quantity).toFixed(2)}</span>
-                  <button
-                    onClick={() => removeFromCart(item.id)}
-                    className="text-red-500"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M5 7h14M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3m0 0H9m0 0H5"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </li>
-            ))} */}
-            <button onClick={handleOnclick} className="mt-auto py-4 w-full bg-blue-500 text-white rounded-full">
-              Proceed
-            </button>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className={`fixed inset-0 z-50 ${isCartOpen ? "block" : "hidden"} bg-black bg-opacity-50`}
+				onClick={toggleCart}
+			/>
+			<div
+				className={`fixed top-0 ${isCartOpen ? "right-0" : "-right-full"} w-[500px] h-full bg-white transition-right duration-300 ease-in-out p-6 box-border z-50`}>
+				<div className="flex justify-between items-center mb-4">
+					<h2>Cart</h2>
+					<button className="text-red-600 text-sm" onClick={toggleCart}>
+						X
+					</button>
+				</div>
+				<hr />
+				<div
+					id="cart"
+					className="flex flex-col justify-between h-[88vh] overflow-auto">
+					<table className="mt-2">
+						<tbody className="space-y-6">
+							{cartItems.map((item, index) => (
+								<tr key={item.id}>
+									<td className="py-3 pr-3">
+										<div className="w-16 h-16">
+											<Image
+												src={item.image}
+												alt={item.title}
+												customClasses="max-w-full max-h-full object-contain"
+											/>
+										</div>
+									</td>
+									<td className="py-3 px-3">{item.title}</td>
+									<td className="py-3 px-3">x{item.quantity}</td>
+									<td className="py-3 px-3">
+										${(item.price * item.quantity).toFixed(2)}
+									</td>
+									<td className="py-3 pl-3 text-right">
+										{" "}
+										<button
+											onClick={() => deleteFromCart(item.id)}
+											className="text-red-500">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												width="24"
+												height="24"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke="currentColor"
+												stroke-width="2">
+												<path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M5 7h14M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3m0 0H9m0 0H5"
+												/>
+											</svg>
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+
+					<button
+						onClick={handleOnclick}
+						className="mt-auto py-4 w-full bg-blue-500 text-white rounded-full">
+						Proceed
+					</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Cart;
