55c4486ab0bf2703c803028a7cab1d9c6ad6698e
Muhammad Awais Danish <mr.awaisdanish@gmail.com>
2024-06-11 17:09:34 +0500

    Loader UI Updated

--- src/components/filters/FiltersLoader.jsx
+++ src/components/filters/FiltersLoader.jsx
@@ -5,25 +5,17 @@ const FiltersLoader = () => {
     <div className="px-6 md:max-lg:px-3 py-4 animate-pulse">
       <div className="space-y-4">
         <div className="flex flex-col">
-          <div className="h-4 bg-gray-300 rounded  w-5/6 mb-2"></div>
-          <div className="grid grid-cols-1 gap-2">
-            <div className="h-4 bg-gray-200 rounded w-1/2"></div>
-          </div>
+          <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
         </div>
         <div className="flex flex-col">
-          <div className="h-4 bg-gray-300 rounded  w-5/6 mb-2"></div>
-          <div className="grid grid-cols-1 gap-2">
-            <div className="h-4 bg-gray-200 rounded w-1/2"></div>
-          </div>
+          <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
         </div>
         <div className="flex flex-col">
-          <div className="h-4 bg-gray-300 rounded  w-5/6 mb-2"></div>
-          <div className="grid grid-cols-1 gap-2">
-            <div className="h-4 bg-gray-200 rounded w-1/2"></div>
-          </div>
+          <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
         </div>
-        <div className="h-4 bg-blue-200 rounded w-3/4"></div>
-        <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+        <hr className="my-5" />
+        <div className="h-4 bg-blue-200 rounded w-full"></div>
+        <div className="h-4 bg-gray-200 rounded w-full"></div>
       </div>
     </div>
   );
